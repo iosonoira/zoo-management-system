88e416077739c2f8b08229491f69677a1e46df02
iosonoira <matteo.dessi93@gmail.com>
2026-09-19 17:08:11 +0200

    feat(zms-fe): add enclosure signage design tokens and app shell

--- zms-fe/src/app/app.ts
+++ zms-fe/src/app/app.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, afterNextRender, inject } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { ZooRole } from './core/models/animal';
+import { ROLE_LABELS } from './core/models/labels';
+import { Session } from './core/session/session';
 
 @Component({
-  imports: [RouterOutlet],
   selector: 'app-root',
-  styleUrl: './app.scss',
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
+  styleUrl: './app.scss',
 })
-export class App {}
+export class App {
+  protected readonly session = inject(Session);
+  protected readonly roles = Object.entries(ROLE_LABELS) as [ZooRole, string][];
+
+  constructor() {
+    afterNextRender(() => this.session.restore());
+  }
+
+  protected onRoleChange(event: Event): void {
+    this.session.setRole((event.target as HTMLSelectElement).value as ZooRole);
+  }
+}
