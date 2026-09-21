99c45d78bc7ba8e242835451099147ddcd60abbf
iosonoira <matteo.dessi93@gmail.com>
2026-09-21 17:04:33 +0200

    refactor(zms-fe): turn Session into a port with a demo adapter

--- zms-fe/src/app/app.config.ts
+++ zms-fe/src/app/app.config.ts
@@ -4,6 +4,8 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { routes } from './app.routes';
 import { AnimalApi } from './core/data/animal-api';
 import { MockAnimalApi } from './core/data/mock-animal-api';
+import { DemoSession } from './core/session/demo-session';
+import { Session } from './core/session/session';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,5 +13,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes, withComponentInputBinding(), withViewTransitions({ skipInitialTransition: true })),
     provideClientHydration(),
     { provide: AnimalApi, useClass: MockAnimalApi },
+    { provide: Session, useClass: DemoSession },
   ],
 };
--- zms-fe/src/app/app.spec.ts
+++ zms-fe/src/app/app.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { provideRouter } from '@angular/router';
 import { App } from './app';
+import { DemoSession } from './core/session/demo-session';
+import { Session } from './core/session/session';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
-      providers: [provideRouter([])],
+      providers: [provideRouter([]), { provide: Session, useClass: DemoSession }],
     }).compileComponents();
   });
 
